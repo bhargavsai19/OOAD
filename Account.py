3add03bb8589bfda3ed60c68d3d0c9925dc1f826
G.Bhargavsai <113325286+bhargavsai19@users.noreply.github.com>
2023-11-23 08:06:07 +0530

    Account.py

---
+++ Account.py
@@ -0,0 +1,48 @@
+# coding=System
+
+class Account(object):
+
+  """
+   
+
+  :version:
+  :author:
+  """
+
+  """ ATTRIBUTES
+
+   
+
+  borrow_book  (private)
+
+   
+
+  reserved_book  (private)
+
+   
+
+  return_book  (private)
+
+   
+
+  fine_amount  (public)
+
+  """
+
+  def calculate_price(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+  def calculate_fine(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
